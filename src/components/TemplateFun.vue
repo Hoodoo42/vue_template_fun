450079d74abac75b76c42e44ff95e74e37a736d8
Hoodoo42 <create@hoodooverse.ca>
2022-08-11 18:45:08 -0600

    knowledge check working with mustache syntax, creating variable, and adding that data into our html through js

---
+++ src/components/TemplateFun.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ blog_title }}</h1>
+    <p>{{ blog_content }}</p>
+    <img :src="blog_image_url" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+      blog_title: `First blog`,
+      blog_content: `This is my first blog`,
+      blog_image_url: `https://images.unsplash.com/photo-1485988412941-77a35537dae4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1796&q=80`,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
