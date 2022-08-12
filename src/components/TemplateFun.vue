b548eed423d7553f00cda557e1d68f1631ce8106
Hoodoo42 <create@hoodooverse.ca>
2022-08-11 20:25:07 -0600

    adding loops into the html to grab the data from the js section to display. It definitly helps knowing the vanilla js way of loops, now its just getting used to this.

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -3,11 +3,16 @@
     <!-- adding data binding to seamlessly allow the js and html to work together with less steps than vanilla js -->
     <!-- this v-if is conditional rendering and is looking to check if the value of show_post is true or false -->
     <!-- the button has a form of event listening on it that when clicked will react to the true/false value and either display or remove the blog  -->
-   <article v-if="show_post === true"></article>
-   <h1>{{ blog_title }}</h1>
-    <p>{{ blog_content }}</p>
-    <img :src="blog_image_url" alt="" />
     <button @click="toggleBlog">on/off</button>
+<!-- putting the rendering conditional into a container that will hold all the posts not just one -->
+    <section v-if="show_post === true">
+      <!-- the article has a loop that will loop through the blogs to display each one on the page -->
+      <article v-for="(blog, index) in blogs" :key="index">
+        <h1>{{ blog[`blog_title`] }}</h1>
+        <p>{{ blog[`blog_content`] }}</p>
+        <img :src="blog[`blog_image_url`]" alt="" />
+      </article>
+    </section>
   </div>
 </template>
 
@@ -15,10 +20,9 @@
 export default {
   //   this is the function that the button is using
   methods: {
-
     // shortcut
     // toggleBlog() this.show_post = !this.show_post
-    
+
     toggleBlog() {
       if (this.show_post === true) {
         this.show_post = false;
@@ -31,10 +35,21 @@ export default {
   data() {
     return {
       //   this is the data being sent up to HTML to diplay on the browser
-      blog_title: `First blog`,
-      blog_content: `This is my first blog`,
-      blog_image_url: `https://images.unsplash.com/photo-1485988412941-77a35537dae4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1796&q=80`,
+    //   show_posts is setting the defualt value to true which will have the blogs displayed until the button is clicked
       show_post: true,
+      blogs: [
+        {
+          blog_title: `First blog`,
+          blog_content: `This is my first blog`,
+          blog_image_url: `https://images.unsplash.com/photo-1485988412941-77a35537dae4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1796&q=80`,
+        },
+
+        {
+          blog_title: `Second blog`,
+          blog_content: `This is my second blog`,
+          blog_image_url: `https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80`,
+        },
+      ],
     };
   },
 };
